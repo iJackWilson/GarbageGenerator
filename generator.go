d150ad233077ce0fe1334af1f25ce6a5bc8702cd
Jack Wilson <jack.wilson8796@gmail.com>
2019-03-16 21:15:45 +0000

    Now takes arguments from flags

--- generator.go
+++ generator.go
@@ -3,21 +3,24 @@ package main
 import ("fmt"
 	"log"
 	"os"
-	"strings")
+	"strings"
+	"flag")
 
 func main() {
-    var i int
-    fmt.Println("Enter length of garbage: ")
-    fmt.Scan(&i)
-    fmt.Println("Creating", i, "characters of garbage")
-    a2 := strings.Repeat("A", i)
-    //fmt.Println(a2)
-    file, err := os.Create("result.txt")
+    filenamePtr := flag.String("filename", "output.txt", "Name of output file")
+    lengthPtr := flag.Int("length", 100, "Length of garbage")
+
+    flag.Parse()
+
+    fmt.Println("Creating", *lengthPtr, "characters of garbage and outputting to", *filenamePtr)
+
+    a2 := strings.Repeat("A", *lengthPtr)
+
+    file, err := os.Create(*filenamePtr)
     if err != nil {
-        log.Fatal("Cannot create file", err)
+	    log.Fatal("Cannot create file", err)
     }
     defer file.Close()
 
     fmt.Fprintf(file, a2)
-
 }
